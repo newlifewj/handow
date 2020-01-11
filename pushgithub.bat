6f4fde6152f531a092918d04358fae2edc89f952
Jian Wang <jian.wang@sentacaconsulting.com>
2020-01-11 17:18:58 -0500

    try fix pushgithub.bat issue, no carrier return in bat

--- pushgithub.bat
+++ pushgithub.bat
@@ -1,7 +1,1 @@
-ren doc _doc
-&& git add -A
-&& git commit -m "Prepare pushing github repo"
-&& git push github dev:master
-&& git fetch origin
-&& git reset --hard origin/dev
-&& rmdir /Q /S _doc
+rmdir /Q /S doc && git add -A && git commit -m "Prepare pushing github repo" && git push github dev:master && git fetch origin && git reset --hard origin/dev
