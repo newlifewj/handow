d7886c6b38b0c68d7bb0c9ef2b80be9e330eef66
Jian Wang <jian.wang@sentacaconsulting.com>
2020-01-11 23:31:56 -0500

    Remove /dist folder, improve .gitignore, .npmignore and pushgithub.bat

--- pushgithub.bat
+++ pushgithub.bat
@@ -1,1 +1,6 @@
-rmdir /Q /S doc && git add -A && git commit -m "Prepare pushing github repo" && git push --force github dev:master && git reset --hard origin/dev
+rmdir /Q /S doc && ^
+rmdir /Q /S lib\render\src && ^
+git add -A && ^
+git commit -m "Prepare pushing to github repository" && ^
+git push --force github dev:master && ^
+git reset --hard origin/dev
