952a9350eb08d84c7d4954ae5b0887297db3cfe5
Jian Wang <newlifewj@gmail.com>
2020-02-05 22:05:55 -0500

    Fixed scenario skip bug

--- demo/project/params/urls.params.js
+++ demo/project/params/urls.params.js
@@ -9,7 +9,8 @@ const path = require('path');
  */
 module.exports = {
     ReportURL: 'https://storage.googleapis.com/handow-uat-assets/static/uat-pet-store/index.html',
-    LocalDemoURL: path.join(`${appRoot}`, "demo/demospa/index.html")
+    LocalDemoURL: path.join(`${appRoot}`, "demo/demospa/index.html"),
     // LocalReportURL: 'file:///C:/handow/handow-dev/demotarget/index.html'
+    Handow_Form: "http://www.handow.org/lab/demoform"
    
 };
